ceaa60b5a8a1daa8ead29e807fc042f7e61362cf
Harunur Rashid <142937596+harunur6729@users.noreply.github.com>
2025-03-06 00:06:05 +0600

    delete and post implement

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,14 +1,34 @@
-import React from "react";
 
-const User = ({ user }) => {
+
+const User = ({ user,setUsers ,users}) => {
+  
+  
+  const handleDelete = _id =>{
+    console.log('delete' , _id);
+    fetch(`http://localhost:5000/users/${_id}`,{
+      method:'DELETE',
+    })
+    .then(res=>res.json())
+    .then(data =>{
+      console.log(data);
+      if(data.deletedCount>0){
+        alert('deleted successfully');
+        const remaining = users.filter(user => user._id !== _id);
+        setUsers(remaining)
+      }
+    })
+
+  }
   return (
-    
-        <tr className="odd:bg-gray-800 even:bg-gray-700 text-white">
-            <td className="p-2 border border-gray-300  w-full">{user.name}</td>
-            <td className="p-2 border border-gray-300  w-full"> {user.email}</td>
-            <td className="p-2 border border-gray-300  w-full"> {user.mobile}</td>
-        </tr>
-    
+    <tr className="odd:bg-gray-800 even:bg-gray-700 text-white">
+      <td className="p-2 border border-gray-300  w-full">{user._id}</td>
+      <td className="p-2 border border-gray-300  w-full">{user.name}</td>
+      <td className="p-2 border border-gray-300  w-full"> {user.email}</td>
+      <td className="p-2 border border-gray-300  w-full"> {user.mobile}</td>
+      <td>
+        <button onClick={() =>handleDelete(user._id)} className="bg-red-300 mx-2 px-2 rounded-lg "> Delete</button>
+      </td>
+    </tr>
   );
 };
 
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,18 +1,21 @@
 import { Link, useLoaderData } from "react-router-dom";
 import User from "./User";
 import { FaArrowRight } from "react-icons/fa";
+import { useState } from "react";
 
 const Users = () => {
-  const users = useLoaderData();
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] =useState(loadedUsers)
   console.log(users);
   return (
     <div className="grid grid-cols-12">
         <div className="text-center col-span-10">
       <h1 className="text-2xl font-bold mb-4">Users: {users.length}</h1>
       <div className="flex justify-center">
-        <table className="w-1/2 border border-gray-300 text-left" border="1">
+        <table className="w-12/8 border border-gray-300 text-left" border="1">
           <thead>
             <tr className="bg-gray-700 text-white">
+              <th className="p-2 border border-gray-300">ID</th>
               <th className="p-2 border border-gray-300">Name</th>
               <th className="p-2 border border-gray-300">Email</th>
               <th className="p-2 border border-gray-300">Mobile No</th>
@@ -20,7 +23,7 @@ const Users = () => {
           </thead>
           <tbody>
             {users.map((user) => (
-              <User key={user._id} user={user} />
+              <User key={user._id} user={user} setUsers={setUsers} users={users} />
             ))}
           </tbody>
         </table>
